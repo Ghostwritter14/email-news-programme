e9180607f95fb1e8d2dc4c98ddafd0891d69cfd5
soham <sohamrvd@gmail.com>
2023-04-11 14:50:49 +0100

    refined the code and reused some old code from older prog.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import requests
 
+from send_email import send_email
+
 # api key and url from new api
 api_key = "0e2e91403ee74df68e5440a9dd665bf2"
-url = "https://newsapi.org/v2/everything?q=apple&from=2023-04-10&to=2023-04-10&sortBy=popularity&apiKey=0e2e91403ee74df68e5440a9dd665bf2"
+url = "https://newsapi.org/v2/everything?" \
+      "q=apple&" \
+      "from=2023-04-10&to=2023-04-" \
+      "10&sortBy=popularity&api" \
+      "Key=0e2e91403ee74df68e5440a9dd665bf2" \
+      "&language=en"
 
 # making  the request
 request = requests.get(url)
@@ -11,6 +18,14 @@ request = requests.get(url)
 content = request.json()
 
 # access the article titles with description
-for article in content["articles"]:
-    print(article["title"])
+body = ""
+for article in content["articles"][:10]:
+    if article["title"] is not None:
+        source_name = article['source']['Name'] if article['source'] is not None else 'Unknown Source'
+        article_str = f"Subject: Recent News on Apple\n{article['title']}\n{source_name}\n{article['url']}\n\n"
+        body += article_str
+
+# send email and to avoid error encode in utf8
+body = body.encode("utf-8")
+send_email(message=body)
 
